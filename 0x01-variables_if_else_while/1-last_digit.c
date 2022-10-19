444aac48034e7edc778d5fc4b40badcb501a0eb8
hmariamf <hmariamasres@gmail.com>
2022-10-19 13:12:05 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,8 @@
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
-	if (n % 10 > 5)
+
+if (n % 10 > 5)
 {
-   printf("greater than 5\n");
+	printf("greater than 5\n");
 }
